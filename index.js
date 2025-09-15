b10c2a14860b11f1cb8d4e24cfcbc8557e464ae7
saponeayo67-del <saponeayo67@gmail.com>
2025-09-15 11:42:00 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-// index.js
 const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Route test
+// dire à Express où trouver les fichiers statiques (HTML, CSS…)
+app.use(express.static(path.join(__dirname)));
+
+// route principale → affiche index.html
 app.get("/", (req, res) => {
-  res.send("✅ Ton backend marche !");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// Lancer le serveur
+// lancer le serveur
 app.listen(PORT, () => {
   console.log(`Serveur lancé sur le port ${PORT}`);
 });
